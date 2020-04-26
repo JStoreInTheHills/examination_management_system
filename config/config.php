6a8408e36bb3e507bb336293f8aa88a3d7b43545
jstoreinthehills <salimjjuma@gmail.com>
2020-04-26 11:04:05 +0300

    Added Database Configuration File

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+// DB credentials.
+define('DB_HOST','mysql');
+define('DB_USER','root');
+define('DB_PASS','root');
+define('DB_NAME','ssrms'); // change to my configurations
+
+// Establish database connection.
+try{
+    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e){
+    exit("Error: " . $e->getMessage());
+}
+?>
