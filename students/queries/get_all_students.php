acab9e5d48a4da09bc9bb039cad91a57d35991a3
jstoreinthehills <salimjjuma@gmail.com>
2020-04-26 12:36:46 +0300

    Added DOB on the list of fetched properties

--- students/queries/get_all_students.php
+++ students/queries/get_all_students.php
@@ -5,7 +5,7 @@
 include("../../config/config.php");
 
 // Standard Query to select all students in the database.
-$getStudentsQuery = "SELECT s.StudentName,s.RollId,s.RegDate,s.StudentId,s.Status,c.ClassName,s2.name, TIMESTAMPDIFF(YEAR, s.DOB, CURDATE()) AS age
+$getStudentsQuery = "SELECT s.DOB,s.StudentName,s.RollId,s.RegDate,s.StudentId,s.Status,c.ClassName,s2.name, TIMESTAMPDIFF(YEAR, s.DOB, CURDATE()) AS age
                   FROM tblstudents s join tblclasses c on c.id=s.ClassId JOIN stream s2 on c.stream_id = s2.stream_id";
 
 // Prepare query using procedure stataements
