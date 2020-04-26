093bb4b1e72543b7bb3ffbe798305f85f188f583
jstoreinthehills <salimjjuma@gmail.com>
2020-04-26 11:13:49 +0300

    Added Subject CRUD

---
+++ subjects/queries/add_subject.php
@@ -0,0 +1,55 @@
+<?php
+
+include "../../config/config.php";
+
+
+$errors = array();      // array to hold validation errors
+$data = array();      // array to pass back data
+
+
+if (empty($_POST['subject_name']))
+    $errors['subject_name'] = 'subject_name is required.';
+
+if (empty($_POST['subject_code']))
+    $errors['subject_code'] = 'subject_code ID is required.';
+
+
+// if there are any errors in our errors array, return a success boolean of false
+if (!empty($errors)) {
+
+    $data['success'] = false;
+    $data['message'] = $errors;
+} else {
+
+    $subject_name = $_POST['subject_name'];
+    $subject_code = $_POST['subject_code'];
+
+
+    $sql = "INSERT INTO  tblsubjects(SubjectName, SubjectCode, Creationdate)
+            VALUES(:subject_name,:subject_code,CURRENT_TIMESTAMP)";
+
+    $query = $dbh->prepare($sql);
+
+    $query->bindParam(':subject_name', $subject_name, PDO::PARAM_STR);
+    $query->bindParam(':subject_code', $subject_code, PDO::PARAM_STR);
+
+    $query->execute();
+
+    $lastInsertId = $dbh->lastInsertId();
+
+    // show a message of success and provide a true success variable
+    if ($lastInsertId){
+        $data['success'] = true;
+        $data['message'] = 'Subject Added Successfully';
+    }
+    else{
+        $data['success'] = false;
+        $data['message'] = 'Input Already Exists...Check the name and try again...';
+    }
+
+
+}
+
+echo json_encode($data);
+
+
---
+++ subjects/queries/get_all_subjects.php
@@ -0,0 +1,18 @@
+<?php
+
+// Including the configuration file that will be used here.
+include("../../config/config.php");
+
+// Standard Query to select all students in the database.
+$getStudentsQuery = "SELECT SubjectName,subject_id,SubjectCode,Creationdate FROM tblsubjects";
+
+// Prepare query using procedure stataements
+$query = $dbh->prepare($getStudentsQuery);
+// Execute the prepared statement
+$query->execute();
+
+// Fetch all students as Objects.
+$results = $query->fetchAll(PDO::FETCH_OBJ);
+
+
+echo json_encode($results);
