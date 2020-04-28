98ec94f1b37eab9a3d012634103e842bb471d285
jstoreinthehills <salimjjuma@gmail.com>
2020-04-28 06:49:08 +0300

    Removed Bordered Added Striped CSS Class Attribute

--- class/class.php
+++ class/class.php
@@ -70,7 +70,7 @@
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table table-bordered" id="class_table" width="100%" cellspacing="0">
+                            <table class="table table-striped" id="class_table" width="100%" cellspacing="0">
                                 <thead>
                                 <tr>
                                     <th>Class Name</th>
