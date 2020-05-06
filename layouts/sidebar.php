fe5474250c77da3e9c7f4515bc0e739e87dfe72b
jstoreinthehills <salimjjuma@gmail.com>
2020-05-06 15:25:10 +0300

    Modified FontAwesome Icons  On The Utitities Bar

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -3,13 +3,13 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/index.php">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+        <div class="sidebar-brand-icon">
+            <i class="fas fa-building"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SRMS</div>
+        <div class="sidebar-brand-text mx-3">Munawarah</div>
     </a>
 
-        <!-- Sidebar Toggler (Sidebar) -->
+    <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
@@ -35,8 +35,9 @@
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+            aria-controls="collapseTwo">
+            <i class="fas fa-school"></i>
             <span>School</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
@@ -48,30 +49,27 @@
                 <a class="collapse-item" href="/exam/exam.php">Manage Exams</a>
                 <a class="collapse-item" href="/subjects/subject.php">Manage Subjects</a>
                 <a class="collapse-item" href="/results/results.php">Manage Results</a>
+                <a class="collapse-item" href="/academic_year/year.php">Manage Academic Year</a>
             </div>
         </div>
     </li>
 
     <!-- Nav Item - Utilities Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fas fa-certificate"></i>
             <span>Reports</span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Student Reports:</h6>
+                <a class="collapse-item" href="/reports/reports.html">Reports</a>
                 <a class="collapse-item" href="/reports/students/all_students.php">All Students</a>
-               <a class="collapse-item" href="#">Student By Class</a>
+                <a class="collapse-item" href="/reports/students/get_student_by_class.php">Student By Class</a>
             </div>
         </div>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Class Reports:</h6>
-               <a class="collapse-item" href="/reports/class/index.php">All Classes</a>
-
-            </div>
-        </div>
+    
     </li>
 
     <!-- Divider -->
@@ -84,7 +82,8 @@
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
+            aria-controls="collapsePages">
             <i class="fas fa-fw fa-folder"></i>
             <span>Result</span>
         </a>
--- layouts/topbar.php
+++ layouts/topbar.php
@@ -170,7 +170,7 @@
                 </a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="" data-toggle="modal" data-target="#logoutModal">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-danger"></i>
                     Logout
                 </a>
             </div>
