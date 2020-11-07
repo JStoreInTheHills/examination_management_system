88bbf95282d3f401e54cb797df7a6aa3a22009dd
jstoreinthehills <salimjjuma@gmail.com>
2020-11-07 13:20:16 +0300

    Added a link to the Exam Table

---
+++ exam/pages/exam_view.php
@@ -0,0 +1,174 @@
+<?php 
+include "../../layouts/utils/redirect.php";
+
+if(!isset($_SESSION['alogin']) || (time() - $_SESSION['last_login_timestamp']) > 1500 || !isset($_SESSION['role_id'])){
+  redirectToHomePage();
+}else{
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title id="page_title"></title>
+
+    <!-- Custom fonts for this template -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link href="../../vendor/fontawesome-free/css/all.css" rel="stylesheet" type="text/css">
+    <link href="../../dist/css/main.min.css" rel="stylesheet" type="text/css">
+
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <?php include "../../layouts/sidebar.php"; ?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <?php include '../../layouts/topbar.php' ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-4 text-gray-800" id="page_heading"></h1>
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <button class="btn btn-outline-primary" id="editExamStatusButton"></button>
+                            <button class="btn btn-primary" id="editExamDetails"></button>
+                        </div>
+
+                    </div>
+                    <nav aria-label="breadcrumb mb-3">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="/index">Home</a></li>
+                            <li class="breadcrumb-item"><a href="/exam/exam">All Exams</a></li>
+                            <li class="breadcrumb-item active" id="exam_nav_label" aria-current="page"></li>
+                        </ol>
+                    </nav>
+                    <span id="alert"></span>
+
+                    <div class="row">
+
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Exam Out Of</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800" id="exam_out_of"></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <a id="editMarksOutOf"><i class="fas fa-edit fa-2x text-primary"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <!-- <div class="col-lg-12">
+                            <div class="card mb-4 shadow ">
+                                <div class="card-header text-primary">
+                                    All Examinations
+                                </div>
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped" width="100%" cellspacing="0">
+                                            <thead>
+                                                <tr>
+                                                    <th>Publish Date</th>
+                                                    <th>Exam Name</th>
+                                                    <th>Out of</th>
+                                                    <th>Created By</th>
+                                                    <th>Actions</th>
+                                                </tr>
+                                            </thead>
+
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div> -->
+                    </div>
+
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <?php include "../../layouts/footer.php";?>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-primary" id="exampleModalCenterTitle"></h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="edit_exam_form" action="" method="POST">
+                            <input type="hidden" name="exam_id_edit" id="exam_id_edit">
+                            
+                            <label for="exam_name" class="text-primary">Enter exam Name: </label>
+                            <div class="form-group row">
+                                <div class="col-sm-12 mb-3 mb-sm-0">
+                                    <input type="text" id="exam_name" class="form-control" name="exam_name">
+                                </div>
+                            </div>
+                            <label for="exam_out_of" class="text-primary">Enter Exam Out Of: </label>
+                            <div class="form-group row">
+                                <div class="col-sm-12 mb-3 mb-sm-0">
+                                    <input type="text" id="exam_out_of_edit" class="form-control" name="exam_out_of_edit">
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- End of Page Wrapper -->
+    <?php require_once('../../layouts/utils/logout_modal.html')?>
+
+    <script src="/dist/js/main.min.js"></script>
+    <script src="/dist/js/utils/utils.js"></script>
+    <script src="/dist/js/exams/exam_view.js"></script>
+</body>
+
+</html>
+
+<?php } ?>
--- src/js/exam/exam.js
+++ src/js/exam/exam.js
@@ -1,127 +1,131 @@
-$(document).ready(() => {
-  const exam_table = $("#exam_table").DataTable({
-    ajax: {
-      url: "./queries/get_all_exams.php",
-      dataSrc: "",
-      type: "GET",
+const exam_table = $("#exam_table").DataTable({
+  ajax: {
+    url: "./queries/get_all_exams.php",
+    dataSrc: "",
+    type: "GET",
+  },
+  columnDefs: [
+    {
+      targets: 1,
+      data: {
+        exam_name: "exam_name",
+        exam_id: "exam_id",
+      },
+      render: (data) => {
+        return `<a href="./pages/exam_view?eid=${data.exam_id}">${data.exam_name}</a>`;
+      },
     },
-    columnDefs: [
-      {
-        targets: 1,
-        data: "exam_name",
-      },
-      {
-        targets: 0,
-        data: "created_at",
-      },
-      {
-        targets: 2,
-        data: "exam_out_of",
-      },
-      {
-        targets: 3,
-        data: "created_by",
-      },
-      {
-        targets: 4,
-        orderable: false,
-        data: "exam_id",
-        render: function (data) {
-          return `<a style="color:red" onClick="deleteExam(${data})" class="fas fa-trash"></a>`;
-        },
-      },
-    ],
-  });
-
-  const toast = {
-    question: function () {
-      return new Promise(function (resolve) {
-        iziToast.question({
-          title: "Warning",
-          icon: "fas fa-exclamation-triangle",
-          message: "Are you sure you want to delete this Exam?",
-          timeout: 20000,
-          close: false,
-          position: "center",
-          buttons: [
-            [
-              "<button><b>YES</b></button>",
-              function (instance, toast, button, e, inputs) {
-                instance.hide({ transitionOut: "fadeOut" }, toast, "button");
-                resolve();
-              },
-              false,
-            ],
-            [
-              "<button>NO</button>",
-              function (instance, toast, button, e, inputs) {
-                instance.hide({ transitionOut: "fadeOut" }, toast, "button");
-              },
-            ],
-          ],
-        });
-      });
+    {
+      targets: 0,
+      data: "created_at",
     },
-  };
-
-  function deleteExam(exam_id) {
-    toast.question().then(function () {
-      $.ajax({
-        url: "./queries/delete_exam.php",
-        type: "POST",
-        data: {
-          exam_id: exam_id,
-        },
-      }).done(function (response) {
-        iziToast.success({
-          type: "Success",
-          icon: "fas fa-certificate",
-          position: "bottomRight",
-          message: "Exam Deleted Successfully",
-          onClosing: function () {
-            exam_table.ajax.reload(null, false);
-          },
-        });
-      });
-    });
-  }
-
-  $("#submit").click((e) => {
-    var formData = {
-      exam_name: $("#exam_name").val(),
-      exam_out_of: $("#exam_out_of").val(),
-    };
-    $.ajax({
-      url: "./queries/add_exam.php",
-      method: "POST",
-      data: formData,
-    }).done(function (response) {
-      var arr = JSON.parse(response);
-      if (arr.success === true) {
-        iziToast.success({
-          title: "Success",
-          icon: "fas fa-certificate",
-          position: "topRight",
-          message: arr.message,
-          onClosing: function () {
-            exam_table.ajax.reload(null, false);
-            $("#exam_form").each(function () {
-              this.reset();
-            });
-          },
-        });
-      } else {
-        iziToast.error({
-          title: "Error",
-          position: "topRight",
-          message: arr.message,
-        });
-      }
-    });
-    e.preventDefault();
-  });
-
-  setInterval(function () {
-    exam_table.ajax.reload(null, false);
-  }, 100000);
+    {
+      targets: 2,
+      data: "exam_out_of",
+    },
+    {
+      targets: 3,
+      data: "created_by",
+    },
+    {
+      targets: 4,
+      orderable: false,
+      data: "exam_id",
+      render: function (data) {
+        return `<a style="color:red" onClick="deleteExam(${data})" class="fas fa-trash"></a>`;
+      },
+    },
+  ],
 });
+
+const toast = {
+  question: function () {
+    return new Promise(function (resolve) {
+      iziToast.question({
+        title: "Warning",
+        icon: "fas fa-exclamation-triangle",
+        message: "Are you sure you want to delete this Exam?",
+        timeout: 20000,
+        close: false,
+        position: "center",
+        buttons: [
+          [
+            "<button><b>YES</b></button>",
+            function (instance, toast, button, e, inputs) {
+              instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+              resolve();
+            },
+            false,
+          ],
+          [
+            "<button>NO</button>",
+            function (instance, toast, button, e, inputs) {
+              instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+            },
+          ],
+        ],
+      });
+    });
+  },
+};
+
+function deleteExam(exam_id) {
+  toast.question().then(function () {
+    $.ajax({
+      url: "./queries/delete_exam.php",
+      type: "POST",
+      data: {
+        exam_id: exam_id,
+      },
+    }).done(function (response) {
+      iziToast.success({
+        type: "Success",
+        icon: "fas fa-certificate",
+        position: "bottomRight",
+        message: "Exam Deleted Successfully",
+        onClosing: function () {
+          exam_table.ajax.reload(null, false);
+        },
+      });
+    });
+  });
+}
+
+$("#submit").click((e) => {
+  var formData = {
+    exam_name: $("#exam_name").val(),
+    exam_out_of: $("#exam_out_of").val(),
+  };
+  $.ajax({
+    url: "./queries/add_exam.php",
+    method: "POST",
+    data: formData,
+  }).done(function (response) {
+    var arr = JSON.parse(response);
+    if (arr.success === true) {
+      iziToast.success({
+        title: "Success",
+        icon: "fas fa-certificate",
+        position: "topRight",
+        message: arr.message,
+        onClosing: function () {
+          exam_table.ajax.reload(null, false);
+          $("#exam_form").each(function () {
+            this.reset();
+          });
+        },
+      });
+    } else {
+      iziToast.error({
+        title: "Error",
+        position: "topRight",
+        message: arr.message,
+      });
+    }
+  });
+  e.preventDefault();
+});
+
+setInterval(function () {
+  exam_table.ajax.reload(null, false);
+}, 100000);
