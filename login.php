990cad602744d7f341ddd1ed3bf454d46f418f46
jstoreinthehills <salimjjuma@gmail.com>
2020-11-09 10:59:26 +0300

    Added check for role_id during login attempt

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 
 session_start();
 // check to see if the session has been started and unset the active session.
-if(isset($_SESSION['alogin'])){
+if(isset($_SESSION['alogin']) || isset($_SESSION['role_id'])){
   unset($_SESSION['alogin']);
   unset($_SESSION['last_login_timestamp']);
   unset($_SESSION['role_id']);
