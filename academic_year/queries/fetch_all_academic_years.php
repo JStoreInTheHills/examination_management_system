9d469616fbf099bf2bf7c49926d4d54d68de06ff
jstoreinthehills <salimjjuma@gmail.com>
2020-11-21 02:12:19 +0300

    Refactor the year fetch module

--- academic_year/queries/fetch_all_academic_years.php
+++ academic_year/queries/fetch_all_academic_years.php
@@ -1,11 +1,15 @@
-<?php include '../../config/config.php';
+<?php 
+
+include '../../config/config.php';
 
 $sql = "SELECT year_id, year_name, created_at, status
         FROM year 
         ORDER BY year_name DESC";
+        
 $query = $dbh->prepare($sql);
 
 $query->execute();
 $result = $query->fetchAll(PDO::FETCH_OBJ);
 
 echo json_encode($result);
+?>
