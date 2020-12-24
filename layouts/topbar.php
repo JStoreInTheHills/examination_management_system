845cc9e7051fee7ebdfe6993419670ff4127005e
jstoreinthehills <salimjjuma@gmail.com>
2020-12-24 15:41:27 +0300

    Removal of the search bar

--- layouts/topbar.php
+++ layouts/topbar.php
@@ -2,20 +2,6 @@
 <!-- Topbar -->
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-3 static-top shadow">
 
-    <!-- Topbar Search -->
-    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-        <div class="input-group">
-            <input type="text" class="form-control bg-light border-1 small" 
-                    placeholder="Click to search for anything"
-                aria-label="Search" aria-describedby="basic-addon2">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                    <i class="fas fa-search fa-sm"></i>
-                </button>
-            </div>
-        </div>
-    </form>
-
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
         <!-- Nav Item - Messages -->
