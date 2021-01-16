932e459b17b295553da866008b4da79b166f5dd9
jstoreinthehills <salimjjuma@gmail.com>
2021-01-16 08:36:24 +0300

    undoing magic quotes from config file

--- config/config.php
+++ config/config.php
@@ -1,15 +1,51 @@
 <?php
-// DB credentials.
-define('DB_HOST','mysql');
-define('DB_USER','root');
-define('DB_PASS','root');
-define('DB_NAME','ssrms'); // change to my configurations
 
-// Establish database connection.
-try{
-    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e){
-    exit("Error: " . $e->getMessage());
-}
-?>
+    // DB credentials.
+    define('DB_HOST','mysql');
+    define('DB_USER','root');
+    define('DB_PASS','root');
+    define('DB_NAME','ssrms');
+
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+
+    // Establish database connection.
+    try{
+    
+        $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS, $options);
+    
+    }catch (PDOException $e){
+    
+        exit("Error: Connection to the database has been closed");
+    
+    }
+
+    // This block of code is used to undo magic quotes.  Magic quotes are a terrible 
+    // feature that was removed from PHP as of PHP 5.4.  However, older installations 
+    // of PHP may still have magic quotes enabled and this code is necessary to 
+    // prevent them from causing problems.  For more information on magic quotes: 
+    // http://php.net/manual/en/security.magicquotes.php 
+    if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) 
+    { 
+        function undo_magic_quotes_gpc(&$array) 
+        { 
+            foreach($array as &$value) 
+            { 
+                if(is_array($value)) 
+                { 
+                    undo_magic_quotes_gpc($value); 
+                } 
+                else 
+                { 
+                    $value = stripslashes($value); 
+                } 
+            } 
+        } 
+    
+        undo_magic_quotes_gpc($_POST); 
+        undo_magic_quotes_gpc($_GET); 
+        undo_magic_quotes_gpc($_COOKIE); 
+    } 
+
+    // This tells the web browser that your content is encoded using UTF-8 
+    // and that it should submit content back to you using UTF-8 
+    header('Content-Type: text/html; charset=utf-8'); 
