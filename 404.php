ce836b48fa67f328366e62c1c36d4ba12ee3f90d
jstoreinthehills <salimjjuma@gmail.com>
2021-03-02 15:11:12 +0300

    Added custom 404 page.

---
+++ 404.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include "./_partials/css_files.php";  ?>
+</head>
+
+<body class="dark ">
+    <div class="wrapper vh-100">
+        <div class="align-items-center h-100 d-flex w-50 mx-auto">
+            <div class="mx-auto text-center">
+                <div class="error mx-auto" data-text="404">404</div>
+                <p class="lead text-gray-800 mb-5">Page Not Found</p>
+                <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
+                <a href="login">&larr; Back to Dashboard</a>
+
+            </div>
+        </div>
+    </div>
+    <script src="/dist/js/main.min.js"></script>
+
+</body>
+
+</html>
+</body>
+
+</html>
